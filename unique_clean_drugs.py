06f29a0d7df8c2dd70f85645c5b72a15d49122a7
Larry Chen <66997456+LarryC01@users.noreply.github.com>
2022-07-21 10:58:00 -0500

    Optimized version via numba jit

--- unique_clean_drugs.py
+++ unique_clean_drugs.py
@@ -9,6 +9,12 @@ from pandas import *
 
 from googletrans import Translator
 
+from numba import jit
+
+import time
+
+start = time.time()
+
 # open the CleanDataBase
 file = xlrd.open_workbook("/home/larrychen/MSFinance/summer2022/MLMapping/MLMapping/localmapping/CleanDataBase.xls")
 
@@ -35,7 +41,7 @@ print(type(row0), type(col12))
 
 unique_catalog_names = sorted(list(set(col12)))[1:]
 
-print(len(unique_catalog_names), len(col12), unique_catalog_names)
+print(len(unique_catalog_names), len(col12))
 
 new_wb = xlwt.Workbook()
 
@@ -44,9 +50,18 @@ sheet = new_wb.add_sheet("drugs")
 sheet.write(0, 0, 'catalog of drugs')
 sheet.write(0, 1, 'República de Chile original')
 sheet.write(0, 2, 'República de Chile matched with catalog')
+sheet.write(0, 3, 'República de Chile original en ingles')
 
-for i in range(len(unique_catalog_names)):
-    sheet.write(i + 1, 0, unique_catalog_names[i])
+# this is a helper function
+@jit
+
+def list_to_col(l, n):
+    for i in range(len(l)):
+        sheet.write(i + 1, n, l[i])
+
+# for i in range(len(unique_catalog_names)):
+#     sheet.write(i + 1, 0, unique_catalog_names[i])
+list_to_col(unique_catalog_names, 0)
     
 # new_wb.save("unique_drug_compared_list.xls")
 
@@ -62,22 +77,25 @@ print(fuzz.partial_ratio("123","1234"))
 
 file_chile_1 = read_csv("CHLDiab1.csv", encoding='latin-1')
 
-# sh1_chile_1 = file_chile_1.sheet_by_index(0)
+
 
 col11_chile_1 = file_chile_1["Descripcion linea Adquisicion"].tolist() # column "L"
 
-# print(type(col11_chile_1), col11_chile_1)
+
 
 unique_chile_1 = list(filter(None, sorted(list(set(col11_chile_1)))))
 
-print(len(unique_chile_1), len(sorted(list(set(col11_chile_1))))) # A unique list of catalog of drugs from Chile
+print(len(sorted(list(set(col11_chile_1))))) # A unique list of catalog of drugs from Chile
 
 # store them in the second colum in unique_drug_compared_list.xls, under title República de Chile original
 
+# car; cat --> lexi. corrspondence note.
+# cat; dog 
 
-
-for i in range(len(unique_chile_1)):
-    sheet.write(i + 1, 1, unique_chile_1[i])
+list_to_col(unique_chile_1, 1)
+# for i in range(len(unique_chile_1)):
+#     sheet.write(i + 1, 1, unique_chile_1[i])
+print(len(unique_chile_1))
     
     
 # espanol to english
@@ -85,10 +103,17 @@ english_unique_chile_1 = []
 translator = Translator(service_urls=['translate.googleapis.com']) 
 # discarded
 
-for x in unique_chile_1:
-    english_unique_chile_1.append(translator.translate(x, dest='en').text)
+def translist(l1, l2):
+    for x in l1:
+        l2.append(translator.translate(x, dest='en').text)
+
+# for x in unique_chile_1:
+#     english_unique_chile_1.append(translator.translate(x, dest='en').text)
+translist(unique_chile_1, english_unique_chile_1)
     
 print(len(english_unique_chile_1))
+
+
 
 
 chile1_drug_catalog = []
@@ -100,15 +125,30 @@ chile1_drug_catalog = []
 # # Get a list of matches ordered by score, default limit to 5
 # print(process.extractOne(query, choices)[0], type(process.extract(query, choices)))
 
-for x in english_unique_chile_1:
-    chile1_drug_catalog.append(process.extractOne(x, unique_catalog_names)[0])
+
+def listmatch(lx, ly, lz):
+    for x in lx:
+        ly.append(process.extractOne(x, lz)[0])
+
+# for x in english_unique_chile_1:
+#     chile1_drug_catalog.append(process.extractOne(x, unique_catalog_names)[0])
+listmatch(english_unique_chile_1, chile1_drug_catalog, unique_catalog_names)
     
 print(len(chile1_drug_catalog))
 
-for i in range(len(chile1_drug_catalog)):
-    sheet.write(i + 1, 2, chile1_drug_catalog[i])
+# for i in range(len(chile1_drug_catalog)):
+#     sheet.write(i + 1, 2, chile1_drug_catalog[i])
+list_to_col(chile1_drug_catalog, 2)
+    
+# for i in range(len(english_unique_chile_1)):
+#     sheet.write(i + 1, 3, english_unique_chile_1[i])
+list_to_col(english_unique_chile_1, 3)
 
 new_wb.save("unique_drug_compared_list.xls")
+
+end = time.time()
+
+print(f"This program runs for {end - start} seconds, which means {(end - start) / 60} minutes")
 
 
 
